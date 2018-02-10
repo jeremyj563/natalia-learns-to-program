7452b9c584030326b8c76d37f23d68d3c5ba967e
Natalia Johnson <kittyfluffbunny@outlook.com>
2018-02-10 14:08:31 -0600

    finished

--- MinnieMouseClock/MinnieMouseClock/Form1.Designer.vb
+++ MinnieMouseClock/MinnieMouseClock/Form1.Designer.vb
@@ -22,9 +22,42 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(283, 259)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Timer1 As Timer
 End Class
--- MinnieMouseClock/MinnieMouseClock/Form1.vb
+++ MinnieMouseClock/MinnieMouseClock/Form1.vb
@@ -1,3 +1,69 @@
 ﻿Public Class Form1
+    Private drag As Boolean
+    Private mousex As Integer
+    Private mousey As Integer
 
+    Private Sub PictureBox1_MouseDown(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseDown
+        drag = True
+        mousex = Cursor.Position.X - Me.Left
+        mousey = Cursor.Position.Y - Me.Top
+    End Sub
+
+    Private Sub PictureBox1_MouseMove(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseMove
+        If drag = True Then
+            Me.Top = Cursor.Position.Y - mousey
+            Me.Left = Cursor.Position.X - mousex
+        End If
+    End Sub
+
+    Private Sub PictureBox1_MouseUp(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseUp
+        drag = False
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Me.TransparencyKey = Color.Red
+        Me.BackColor = Color.Red
+        Timer1.Interval = 1000
+        Timer1.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Me.Refresh()
+    End Sub
+
+    Private Sub PictureBox1_Paint(sender As Object, e As PaintEventArgs) Handles PictureBox1.Paint
+        Dim g = e.Graphics
+        Dim canvasWidth = PictureBox1.Width
+        Dim canvasHeight = PictureBox1.Height
+        Dim centerX = canvasWidth / 2
+        Dim centerY = canvasHeight / 2
+        Dim diameter = Math.Min(canvasWidth, canvasHeight) - 2
+        Dim radius = diameter / 2
+        Dim currentTime = Now.TimeOfDay
+        Dim hourPen As New Pen(Color.HotPink, 8)
+        Dim minutePen As New Pen(Color.Blue, 6)
+        Dim secondPen As New Pen(Color.Purple, 4)
+
+        g.TranslateTransform(centerX, centerY)
+
+        ' Draw the hours hand
+        Dim hourAngle = currentTime.TotalMinutes / 2
+        g.RotateTransform(hourAngle)
+        g.DrawLine(hourPen, Point.Empty, New Point(0, -(radius * 2 / 3)))
+
+        ' Draw the minutes hand
+        Dim minuteAngle = currentTime.Minutes * 6
+        g.RotateTransform(minuteAngle)
+        g.DrawLine(minutePen, Point.Empty, New Point(0, -(radius * 4 / 5)))
+
+        ' Draw the seconds hand
+        Dim secondAngle = currentTime.Seconds * 6
+        g.RotateTransform(secondAngle)
+        g.DrawLine(secondPen, Point.Empty, New Point(0, -(radius)))
+
+    End Sub
+
+    Private Sub PictureBox1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseDoubleClick
+        Application.Exit()
+    End Sub
 End Class
